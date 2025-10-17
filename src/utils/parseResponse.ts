22fb226707dc8141d72d3b0e1b567e3eee026c28
João Fernandes <joao.fernandes-terceiro@cea.com.br>
2025-10-17 16:54:59 -0300

    httpApi + cors

--- src/utils/parseResponse.ts
+++ src/utils/parseResponse.ts
@@ -1,8 +1,14 @@
 import { HttpResponse } from "../types/Http";
 
 export function parseResponse({statusCode, body}: HttpResponse) {
-  return { 
+  return {
     statusCode,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type,Authorization',
+      'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+      'Content-Type': 'application/json'
+    },
     body: body ? JSON.stringify(body): undefined
   }
 }
